eaba918b77a727abf872f4fe41bb459dc8123900
gaofei <scofieldgao@qq.com>
2015-06-17 15:56:11 +0800

    thread demo

--- test/thread.cpp
+++ test/thread.cpp
@@ -1,3 +1,4 @@
+#include "TTGlobal.h"
 #include "TTCommonThread.h"
 
 class CTestThread : public CThread
@@ -5,13 +6,60 @@ class CTestThread : public CThread
 public:
 	CTestThread()
 	{
+		m_nCount = 0;
 	}
-	~CTestThread()
+	virtual ~CTestThread()
 	{
 
 	}
 public:
-
+	virtual void Execute();
 private:
-
+	int32_t m_nCount;
 };
+
+void CTestThread::Execute()
+{
+	while (!GetTerminated())
+	{
+		printf("%s, count%d\n", GetName(), m_nCount++);
+		usleep(1000000);
+	}
+	pthread_exit(NULL);
+}
+
+int main()
+{
+	int32_t nThreadCount = 5;
+	CTestThread testThreads[nThreadCount];
+	for (int32_t i = 0; i < nThreadCount; ++i)
+	{
+		char threadName[64];
+		sprintf(threadName, "thread%d", i + 1);
+		testThreads[i].SetName(threadName);
+		testThreads[i].Start();
+	}
+	while (true)
+	{
+		char in = getchar();
+		if ('t' == in)
+		{
+			for (int32_t i = 0; i < nThreadCount; ++i)
+			{
+				testThreads[i].Terminate();
+			}
+		}
+		else if ('s'==in)
+		{
+			break;
+		}
+		if ('b' == in)
+		{
+			for (int32_t i = 0; i < nThreadCount; ++i)
+			{
+				testThreads[i].Start();
+			}
+		}
+		usleep(1000);
+	}
+}
